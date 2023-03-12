0d34b0c35e16b7a523e7a9886a7e859fdd818104
RexshackGaming <rexshackuk@gmail.com>
2023-03-12 08:34:02 +0000

    Update es.lua

--- locales/es.lua
+++ locales/es.lua
@@ -35,7 +35,10 @@ local Translations = {
     }
 }
 
-Lang = Locale:new({
-    phrases = Translations,
-    warnOnMissing = true
-})
+if GetConvar('rsg_locale', 'en') == 'es' then
+  Lang = Locale:new({
+      phrases = Translations,
+      warnOnMissing = true,
+      fallbackLang = Lang,
+  })
+end
