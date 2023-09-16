9e474e55465113a5fbd611b0c55a671e293fb9eb
Inseltreff <127403786+Inseltreff@users.noreply.github.com>
2023-09-16 08:08:08 +0200

    Update de.lua

--- locales/de.lua
+++ locales/de.lua
@@ -13,7 +13,19 @@ local Translations = {
       crafting_a = 'Handwerk: ',
     },
     label = {
-      open_crafting_menu = 'Waffenwerkstatt-Menü öffnen'
+      open_crafting_menu = 'Waffenschmied-Menü öffnen',
+      parts_crafting = 'Teileherstellung',
+      parts_crafting_sub = 'Waffenkomponenten herstellen',
+      weapon_crafting = 'Waffenherstellung',
+      weapon_crafting_sub = 'Waffen herstellen',
+      ammo_crafting = 'Munitionsherstellung',
+      ammo_crafting_sub = 'Waffenmunition herstellen',
+      weapon_storage = 'Waffenschmied-Lager',
+      weapon_storage_sub = 'Lagerung für Waffenschmiede',
+      repair_weapon = 'Waffe reparieren',
+      repair_weapon_sub = 'Die ausgerüstete Waffe reparieren',
+      explore_options = 'Erkunde die Herstellungsoptionen für ',
+      inspect = 'Ausgerüstete Waffe inspizieren',
     }
 }
 
@@ -21,3 +33,5 @@ Lang = Locale:new({
     phrases = Translations,
     warnOnMissing = true
 })
+
+-- Lang:t('label.inspect')
